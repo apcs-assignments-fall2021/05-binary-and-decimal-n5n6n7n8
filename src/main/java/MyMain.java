bb8723c1728520479b9fc84c42a2dae0cc7dbe79
natewong <nwwong17@gmail.com>
2021-09-14 15:02:23 -0700

    Created binary to decimal and decimal to binary calculator for APCS HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -8,8 +8,19 @@ public class MyMain {
     // Ex. binaryToDecimal("1010") => 10 (ten)
     //     binaryToDecimal("110010") => 50
     public static int binaryToDecimal(String binary) {
-        // REPLACE WITH YOUR CODE
-        return -1;
+        int bin = Integer.parseInt(binary);
+        int sum = 0;
+        int increment=1;
+        while(bin>0)
+        {
+            if(bin%10==1)
+            {
+                sum+=increment;
+            }
+            bin/=10;
+            increment*=2;
+        }
+        return sum;
     }
 
     // Given a decimal number (e.g. 152) as input, and returns a String
@@ -17,13 +28,28 @@ public class MyMain {
     // Ex. decimalToBinary(7) => "111"
     //     decimalToBinary(152) => "10011000"
     public static String decimalToBinary(int decimal) {
-        // REPLACE WITH YOUR CODE
-        return "";
+        if(decimal==0)
+        {
+            return "0";
+        }
+        String ans = "";
+        while (decimal>0)
+        {
+            ans = String.valueOf(decimal%2) + ans;
+            decimal/=2;
+        }
+        return ans;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // YOUR CODE HERE
+        System.out.println("Write a number in binary and I will convert it to an integer.");
+        String binary = scan.nextLine();
+        System.out.println(binaryToDecimal(binary));
+        System.out.println("Write an integer and I will convert it to binary.");
+        int decimal = scan.nextInt();
+        System.out.println(decimalToBinary(decimal));
+        scan.close();
     }
 }
